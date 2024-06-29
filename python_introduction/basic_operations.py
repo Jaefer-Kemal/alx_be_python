ad5cf70d9ecfc1896739a55989500742f9003376
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-06-29 16:02:16 +0300

    Added file named basic_operation.py

---
+++ python_introduction/basic_operations.py
@@ -0,0 +1,9 @@
+number1 = 10
+number2 = 5
+add = number1 + number2
+sub = number1 - number2
+pro = number1 * number2
+
+print(
+    f"Addition of {number1} and {number2} is {add}\nSubtraction of {number1} and {number2} is {sub}\nMultiplication of {number1} and {number2} is {pro}"
+)
