ce9d2b2c698d0dab6ff566b31fae85e28100ad48
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-06-30 12:35:50 +0300

    Added a file simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000 #representing $1000
+rate = 0.05 # representing 5% annual interest rate
+time = 3 # in years
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
