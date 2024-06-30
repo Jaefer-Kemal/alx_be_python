7d552a56ce576c129849e9278db8dfd833b2ff87
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-06-30 13:18:37 +0300

    Modieifed file finance_calculator.py by changing the variable name and introducing float data type to it

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,8 +1,8 @@
-income = int(input("Enter your monthly income: "))
-total_expense = int(input("Enter your total monthly expenses: "))
+monthly_income = float(input("Enter your monthly income: "))
+monthly_expenses = float(input("Enter your total monthly expenses: "))
 
-monthly_savings =  income - total_expense
-print(f"Your monthly savings are ${monthly_savings}.")
+monthly_savings =  monthly_income - monthly_expenses
+print(f"Your monthly savings are ${int(monthly_savings)}.")
 
 projected_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
 
