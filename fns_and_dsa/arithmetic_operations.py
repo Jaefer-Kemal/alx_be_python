cc3c9fc404ec3de11372edb7f1b3a949c4851b8c
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-03 16:17:35 +0300

    Modified file arithmetic.py by using if statement instead of switch case(match)

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,11 +1,26 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2
-        case "subtract":
-            return num1 - num2
-        case "multiply":
-            return num1 * num2
-        case "divide":
-            return num1 / num2
+    # match operation:
+    #     case 'add':
+    #         return num1 + num2
+    #     case "subtract":
+    #         return num1 - num2
+    #     case "multiply":
+    #         return num1 * num2
+    #     case "divide":
+    #         return num1 / num2
+    """It's commented out my first trial because the checker wants "==" sign inside the function """
+    
+    # Second Trial
+    
+    if operation == "add":
+        return num1 + num2
+    
+    elif operation == "subtract":
+        return num1 - num2
+    
+    elif operation == "multiply":
+        return num1 * num2
+    
+    elif operation == "divide":
+        return num1 / num2
         
