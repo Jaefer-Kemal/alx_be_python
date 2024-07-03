8fbc69043d19264d56ba463ea50f438a54a9a109
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-03 21:52:27 +0300

    added a new function from datetime module and removed the for loop functionality in formating the date time

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -4,15 +4,9 @@ from datetime import timedelta
 
 def display_current_datetime():
     current_date = datetime.now()
-    current_date = str(current_date)
-    for i,v in enumerate(current_date):
-        if v ==".":
-            index = i
-            break
-        
-    current_date = f"{current_date[:index]}:{current_date[index+1:index+3]}"
-    return f"Current date and time: {current_date}"
-
+    current_date = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    return current_date
+    
 
 def calculate_future_date():
     number_of_day = int(input("Enter the number of days to add to the current date: "))
