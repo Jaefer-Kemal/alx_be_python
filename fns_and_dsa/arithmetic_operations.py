59317106bad82c3f0c67d6f7c7ac3c064ed091f9
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-03 16:25:52 +0300

    Modified file arithmetic.py by adding addtional condition for division when the denominator is equal to 0

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -24,5 +24,6 @@ def perform_operation(num1, num2, operation):
     elif operation == "divide":
         if num2 != 0:
             return num1 / num2
-       
+        else:
+            return "num2 == 0"
         
