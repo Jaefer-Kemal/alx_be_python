502d13377ab1322248c76b3cb96ab32fcc398edc
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-21 22:07:42 +0300

    Added Library_sysem.py to deepen our understanding of inheritance and composition in python

--- oop/library_system.py
+++ oop/library_system.py
@@ -8,18 +8,18 @@ class Book:
 
 class EBook(Book):
     def __init__(self,title,author,file_size):
+        super().__init__(title,author)
         self.file_size = file_size
-        self.title = title
-        self.author = author
+        
         
     def __str__(self):
         return f"EBook: {self.title} by {self.author}, File Size: {self.file_size}KB"
 
 class PrintBook(Book):
     def __init__(self,title,author,page_count):
+        super().__init__(title,author)
         self.page_count = page_count
-        self.title = title
-        self.author = author
+        
 
     def __str__(self):
         return f"PrintBook: {self.title} by {self.author}, Page Count: {self.page_count}"
