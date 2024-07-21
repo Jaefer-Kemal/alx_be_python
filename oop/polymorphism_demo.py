e80162d7f626938d6f4faf67ef7913e9a29b37c8
Jaefer-Kemal <jaeferkemal@gmail.com>
2024-07-21 22:21:57 +0300

    Added polymorphism.py to demostrate the method overriding and polymorphic behavior

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,22 @@
+from math import pi
+class Shape():
+    def __init__(self):
+        pass
+    def area(self):
+        raise NotImplementedError
+    
+class Rectangle(Shape):
+    def __init__(self,length,width):
+        self.length = length
+        self.width = width
+    
+    def area(self):
+        return self.length * self.width
+    
+    
+class Circle(Shape):
+    def __init__(self,radius):
+        self.radius = radius
+    
+    def area(self):
+        return pi * self.radius * self.radius
